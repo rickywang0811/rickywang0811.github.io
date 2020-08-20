9b0b15eda0f07a0f030c4b2e5893b9c899f5925c
rickywang0811 <xuyouxi@UC-Pro.local>
2020-08-20 23:35:09 +0800

    Add database.js: delete invalid igid

--- database.js
+++ database.js
@@ -11,7 +11,26 @@ new Vue({
   mounted() {
   },
   methods: {
+    delIgId(id) {
+      this.isLoading = true;
+      console.log(`Delete id ${id}`)
+      const api = 'https://riceballweb.herokuapp.com/delInvalidId';
+      // const api = 'http://127.0.0.1:5889/delInvalidId';
+      let body = {
+        id: id,
+      };
+      axios.post(api, body).then(rsp => {
+        console.log(rsp);
+        this.isLoading = false;
+        this.igArr = rsp.data;
+        this.postSchool();
+      }).catch(e => {
+        console.log(e);
+        this.isLoading = false;
+      })
+    },
     postSchool () {
+      console.log(`Get ${this.selected} list`)
       this.isLoading = true;
       const api = 'https://riceballweb.herokuapp.com/getSchoolVoted';
       // const api = 'http://127.0.0.1:5889/getSchoolVoted';
